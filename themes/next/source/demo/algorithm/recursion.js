c4d41d9ee284a45f425fa7d53a474f4649e4f69c
wuwhs <605776026@qq.com>
2020-11-02 14:51:16 +0800

    daily update 2020/11/2

--- themes/next/source/demo/algorithm/recursion.js
+++ themes/next/source/demo/algorithm/recursion.js
@@ -8,5 +8,20 @@ const numDecoding = function (str) {
 const decode = function (chars, index) {
   if (index <= 0) return 1
   let count = 0
-  let curr = chars
+  let curr = chars[index]
+  let prev = chars[index - 1]
+
+  // 当前字符比 `0` 大，则直接利用它之前的字符串所求得结果
+  if (curr > '0') {
+    count = decode(chars, index - 1)
+  }
+
+  // 由前一个字符和当前字符构成的数字，值必须要在1和26之间，否则无法编码
+  if (prev === '1' || (prev == '2' && curr <= '6')) {
+    count += decode(chars, index - 2)
+  }
+
+  return count
 }
+
+console.log('count: ', numDecoding('1213'))
