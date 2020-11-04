d9afbee61abc38a717ff4b828e0bbce9b1ba20e6
wenhua <wenhuawu@sf-express.com>
2020-11-04 20:40:47 +0800

    daily update 2020/11/04

--- themes/next/source/demo/algorithm/recursion.js
+++ themes/next/source/demo/algorithm/recursion.js
@@ -85,3 +85,47 @@
 // }
 
 // console.log(combinationSum([1, 2, 3], 10))
+
+const check = function (row, col, columns) {
+  for (let r = 0; r < row; r++) {
+    // 其他皇后是否在当前放置皇后的列和对角线上
+    if ((columns[r] = col || row - r == Math.abs(columns[r] - col))) {
+      return false
+    }
+  }
+  return true
+}
+
+let count
+const totalNQueens = function (n) {
+  count = 0
+  const results = []
+  backtracking(n, 0, [], [], results)
+  console.log('results: ', results)
+  console.log('count: ', count)
+}
+
+const backtracking = function (n, row, columns, solution, results) {
+  // 是否在所有 n 行里都摆好了皇后
+  if (row === n) {
+    results.push([...solution])
+    count++
+    return
+  }
+
+  // 尝试将皇后放置到当前行中的每一列
+  for (let col = 0; col < n; col++) {
+    columns[row] = col
+    solution.push([row, col])
+
+    // 检查是否合法，如果合法就继续到下一行
+    if (check(row, col, columns)) {
+      backtracking(n, row + 1, columns, solution, results)
+    }
+    solution.pop()
+    // 如果不合法，就不要把皇后放在这列中
+    columns[row] = -1
+  }
+}
+
+totalNQueens(3)
